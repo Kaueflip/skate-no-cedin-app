b6038f5168f50dd27df6d005437ffd8de8a93f98
Kauê Felipe <kauefelipemail@gmail.com>
2026-05-18 00:44:14 -0300

    theme provider update

--- components/theme/theme-provider.tsx
+++ components/theme/theme-provider.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import * as React from "react"
+import * as React
+    from "react"
 
 import {
     ThemeProvider as NextThemesProvider,
@@ -8,7 +9,7 @@ import {
 
 import type {
     ThemeProviderProps,
-} from "next-themes/dist/types"
+} from "next-themes"
 
 export function ThemeProvider({
     children,
